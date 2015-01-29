0fc60512d5bdc052be9ba0ae4fc47955326f8340
Luis Pedraza <luispedraza@gmail.com>
2015-01-29 16:51:10 +0100

    finished adding lifecycle methods and corresponding logs

--- app/src/main/java/es/luispedraza/androidactivitylifecycle/MainActivity.java
+++ app/src/main/java/es/luispedraza/androidactivitylifecycle/MainActivity.java
@@ -1,19 +1,15 @@
 package es.luispedraza.androidactivitylifecycle;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
+    static private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +31,51 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Lifecycle methods:
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v(LOG_TAG, "Executing onCreate");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.v(LOG_TAG, "Executing onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.v(LOG_TAG, "Executing onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.v(LOG_TAG, "Executing onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.v(LOG_TAG, "Executing onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.v(LOG_TAG, "Executing onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.v(LOG_TAG, "Executing onDestroy");
+        super.onDestroy();
     }
 }
